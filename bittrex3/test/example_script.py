fd41424ea01c8237fb3fdcf4992d4973ffd5e65b
Cedric <cedricholz@berkeley.edu>
2017-08-18 15:23:43 -0700

    Updated example script, tested and working

--- bittrex3/test/example_script.py
+++ bittrex3/test/example_script.py
@@ -14,7 +14,6 @@ dollarsToUSD = b.convert_to_btc(1, "USD")
 print("1 USD to bitcoin " + str(dollarsToUSD))
 
 # Get these from https://bittrex.com/Account/ManageApiKey
-#api = Bittrex3('ecf839a28fcf46889cc1f8cc95ec05c6', 'fb0d5e1aee744a7997f3dfa43b18cc57')
 
 with open("secrets.json") as secrets_file:
     secrets = json.load(secrets_file)
@@ -23,14 +22,13 @@ with open("secrets.json") as secrets_file:
 api = Bittrex3(secrets['key'], secrets['secret'])
 
 
-
 # Market to trade at
 trade = 'BTC'
 currency = 'DOGE'
 market = '{0}-{1}'.format(trade, currency)
 
 # Amount of coins to buy
-amount = 1
+amount = 1500
 
 # How big of a profit you want to make
 multiplier = 1.1
@@ -38,23 +36,23 @@ multiplier = 1.1
 # Getting the BTC price for DOGE
 dogesummary = api.get_ticker(market)
 
-dogeprice = dogesummary['result']['Last']
+dogeprice = float(dogesummary['result']['Last'])
 
-print ('The price for {0} is {1:.8f} {2}.'.format(currency, dogeprice, trade))
+print('The price for {0} is {1:.8f} {2}.'.format(currency, dogeprice, trade))
 
 # Buying 100 DOGE for BTC
-print ('Buying {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
-api.buylimit(market, amount, dogeprice)
+print('Buying {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
+api.buy_limit(market, amount, dogeprice)
 
 # Multiplying the price by the multiplier
 dogeprice = round(dogeprice*multiplier, 8)
 
 # Selling 100 DOGE for the  new price
-print ('Selling {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
-api.selllimit(market, amount, dogeprice)
+print('Selling {0} {1} for {2:.8f} {3}.'.format(amount, currency, dogeprice, trade))
+api.sell_limit(market, amount, dogeprice)
 
 # Gets the DOGE balance
-dogebalance = api.getbalance(currency)
-print ("Your balance is {0} {1}.".format(dogebalance['Available'], currency))
+dogebalance = api.get_balance(currency)
+print("Your balance is {0} {1}.".format(dogebalance['result']['Balance'], currency))
 
 # For a full list of functions, check out bittrex.py or https://bittrex.com/Home/Api
