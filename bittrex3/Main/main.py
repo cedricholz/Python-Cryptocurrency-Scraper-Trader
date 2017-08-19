02fb05aef438c38b5128061eb1c947292041a68c
Cedric <cedricholz@berkeley.edu>
2017-08-18 20:25:13 -0700

    syncing

--- bittrex3/Main/main.py
+++ bittrex3/Main/main.py
@@ -1,9 +1,6 @@
 from bittrex3.bittrex3 import Bittrex3
-from forex_python.bitcoin import BtcConverter
 import json
 import urllib
-import requests
-
 
 with open("secrets.json") as secrets_file:
     secrets = json.load(secrets_file)
@@ -13,14 +10,11 @@ with open("secrets.json") as secrets_file:
 api = Bittrex3(secrets['key'], secrets['secret'])
 
 
-with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=50") as url:
+with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=1000") as url:
     coinmarketcap_data = json.loads(url.read().decode())
 
-
-top_fifty_coins = []
-
+top_coins = []
 
 for coin in coinmarketcap_data:
-    top_fifty_coins.append(coin['id'])
-    
-print(top_fifty_coins)
+    top_coins.append(coin['id'])
+
