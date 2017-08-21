0cea1dd3e0d6c03ca3bf28ae0b54968e8497557c
Cedric <cedricholz@berkeley.edu>
2017-08-20 18:23:15 -0700

    changed name of file

---
+++ Trader/examples/example_reddit_bot.py
@@ -0,0 +1,46 @@
+import praw
+import json
+import requests
+
+with open("reddit_secrets.json") as secrets_file:
+    reddit_secrets = json.load(secrets_file)
+    secrets_file.close()
+
+reddit = praw.Reddit(client_id=reddit_secrets["client_id"], client_secret=reddit_secrets["client_secret"],
+                     password=reddit_secrets["password"], user_agent=reddit_secrets["user_agent"],
+                     username=reddit_secrets["username"])
+
+# Check to see OAuth worked
+print(str(reddit.user.me()) + '\n')
+
+
+# Print the first "limit" titles on r/cryptocurrency and print their sentiment
+for submission in reddit.subreddit('CryptoCurrency').hot(limit=25):
+    print(submission.title)
+
+    for x in submission.comments:
+        print(x.body)
+
+    r = requests.post("http://text-processing.com/api/sentiment/", data={'text': submission.title})
+    processedText = json.loads(r.text)
+
+    pos = processedText['probability']['pos']
+    neutral = processedText['probability']['neutral']
+    neg = processedText['probability']['neg']
+
+    sentiment_pairs = {"Positive": pos, "Neutral": neutral, "Negative": neg}
+
+    v = list(sentiment_pairs.values())
+    k = list(sentiment_pairs.keys())
+    print("Mostly " + k[v.index(max(v))])
+
+    print("Positive " + str(pos))
+    print("Neutral " + str(neutral))
+    print("Negative " + str(neg) + "\n")
+
+
+
+
+
+
+
---
+++ Trader/utils.py
@@ -0,0 +1,101 @@
+from forex_python.bitcoin import BtcConverter
+from Trader.bittrex3 import Bittrex3
+from datetime import datetime, timezone
+import urllib
+import json
+import re
+
+
+def file_to_json(filename):
+    try:
+        file = open(filename, 'r')
+        contents = str(file.read())
+        posts = json.loads(contents)
+        file.close()
+        return posts
+    except FileNotFoundError:
+        print("No such file: " + filename)
+        exit(1)
+
+
+def json_to_file(json_obj, filename):
+    try:
+        file = open(filename, 'w')
+        json.dump(json_obj, file, sort_keys=True, indent=4, separators=(',', ': '))
+        file.close()
+    except FileNotFoundError:
+        print("No such file: " + filename)
+        exit(1)
+
+
+def get_percent_change_24h(coin):
+    return (coin['Last'] / coin['PrevDay'] - 1) * 100
+
+
+def bitcoin_to_USD(bitcoin_amount):
+    b = BtcConverter()
+    latest_price = b.get_latest_price('USD')
+    return latest_price * bitcoin_amount
+
+
+def get_api():
+    with open("secrets.json") as secrets_file:
+        secrets = json.load(secrets_file)
+        secrets_file.close()
+
+    return Bittrex3(secrets['key'], secrets['secret'])
+
+
+def query_url(url_addr):
+    with urllib.request.urlopen(url_addr) as url:
+        return json.loads(url.read().decode())
+
+
+def get_first_market_coin(market):
+    m = re.search('([A-z]+)-([A-z]+)', market)
+    return m.group(1)
+
+
+def get_second_market_coin(market):
+    m = re.search('([A-z]+)-([A-z]+)', market)
+    return m.group(2)
+
+
+def get_coin_market_cap_1hr_change():
+    coinmarketcap_coins = query_url("https://api.coinmarketcap.com/v1/ticker/?limit=2000")
+    d = {}
+    for coin in coinmarketcap_coins:
+        d[coin['symbol']] = coin['percent_change_1h']
+    return d
+
+
+def print_and_write_to_logfile(log_text):
+    print(log_text + "\n")
+    with open("logs.txt", "a") as myfile:
+        myfile.write(log_text + "\n\n")
+
+
+def get_total_bitcoin(api):
+    try:
+        return float(api.get_balance('BTC')['result']['Available'])
+    except LookupError:
+        return 0
+
+
+def get_date_time():
+    now = datetime.now()
+    return "%s:%s:%s %s/%s/%s" % (now.hour, now.minute, now.second, now.month, now.day, now.year)
+
+
+def get_time_passed_minutes(time_opened):
+    now = datetime.now()
+
+    time_offset = datetime.utcnow() - now
+
+    opened_datetime = datetime.strptime(time_opened, "%Y-%m-%dT%H:%M:%S.%f") - time_offset
+
+    time_diff = now - opened_datetime
+
+    time_passed = time_diff.total_seconds()/60
+
+    return time_passed
