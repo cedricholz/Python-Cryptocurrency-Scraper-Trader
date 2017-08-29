284d5591c4a537e3abd8aad7f5807b48c77fa00b
Cedric Holz <cedricholz@gmail.com>
2017-08-29 01:15:25 -0700

    added scripts for linux

---
+++ Trader/Main/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo '{
+    "Buying": {},
+    "Selling": {}
+}' > pending_orders.json
+
+
+echo '{
+}' > coin_highest_price_history.json
+
+
+echo '{
+}' > held_coins.json
+
+echo '{
+}' > cat global_return.json
+
